e886a875bfd93512f4ded1f727a3399390b4d082
David Snider <dsnider.techie@gmail.com>
2017-09-20 09:28:29 -0500

    new file:   EmailBot.py

---
+++ EmailBot.py
@@ -0,0 +1,12 @@
+#David Snider
+#started 9/19/17 at 7:00am
+#EmailBot.py
+""" This program is to simplify the job of the Out Till Midnight Committee
+    of the Beta Eta chapter of Tau Kappa Epsilon """
+
+
+
+
+
+if __name__ == "__main__":
+    print("Bot complete")
